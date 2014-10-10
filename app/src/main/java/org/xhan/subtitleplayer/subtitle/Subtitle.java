783c561ab37ddbb58d834b671e4391089461b6b0
xhan <hhhanxf@gmail.com>
2014-10-09 23:11:46 -0700

    Subtitle data object

--- app/src/main/java/org/xhan/subtitleplayer/subtitle/Subtitle.java
+++ app/src/main/java/org/xhan/subtitleplayer/subtitle/Subtitle.java
@@ -1,7 +1,10 @@
 package org.xhan.subtitleplayer.subtitle;
 
+import java.util.List;
+
 /**
  * Created by xhan on 10/9/14.
  */
 public class Subtitle {
+    List<SubtitleLine> lines;
 }
--- app/src/main/java/org/xhan/subtitleplayer/subtitle/SubtitleLine.java
+++ app/src/main/java/org/xhan/subtitleplayer/subtitle/SubtitleLine.java
@@ -1,9 +1,46 @@
 package org.xhan.subtitleplayer.subtitle;
 
+import java.sql.Timestamp;
+
 /**
  * Created by xhan on 10/9/14.
  */
 public class SubtitleLine {
     private int index;
-    private
+    private Timestamp timestamp;
+    private String content;
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("SubtitleLine{");
+        sb.append("index=").append(index);
+        sb.append(", timestamp=").append(timestamp);
+        sb.append(", content='").append(content).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
 }
